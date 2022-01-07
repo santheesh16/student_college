89be63dda44e30049af39a52ee6d6516368d4ce3
Santheesh <55356327+santheesh16@users.noreply.github.com>
2022-01-07 12:46:25 +0530

    deploy

--- server.js
+++ server.js
@@ -30,10 +30,10 @@ app.use('/api', excelRoutes);
 app.use('/api', labRoutes);
 
 if(process.env.NODE_ENV === 'production'){
-  app.use(express.static(path.join(__dirname,'client/build')));
+  app.use(express.static('client/build'));
 
   app.get('*', function(req, res){
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(__dirname, 'client/build', 'index.html');
   })
 }
 
